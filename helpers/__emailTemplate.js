b5308ae9ce0e6c5611b8d9015eabb38b6dbea547
AlexBinkovskyy <alex_bin@ukr.net>
2024-05-01 13:13:48 +0300

    emailBodyFix#3

--- helpers/__emailTemplate.js
+++ helpers/__emailTemplate.js
@@ -213,7 +213,7 @@ export const emailTemplate = (link) => {
           <!-- start copy -->
           <tr>
             <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-bottom: 3px solid #d4dadf">
-              <p style="margin: 0;">Cheers,<br> Paste</p>
+              <p style="margin: 0;">Cheers, backend team<br>Alex Binkovskyi's Node.js project</p>
             </td>
           </tr>
           <!-- end copy -->
