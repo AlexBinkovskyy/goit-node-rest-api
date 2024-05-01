36fcae9bcfcf50f9a1e63b4243e5d897a72a0936
AlexBinkovskyy <alex_bin@ukr.net>
2024-05-01 16:41:08 +0300

    static page layout done

--- public/html/script.js
+++ public/html/script.js
@@ -1,3 +1,20 @@
-window.onload = ()=>{
-    console.log(window.location.href.split('?')[1]);
-}
+window.onload = async () => {
+  try {
+    const wrapper = document.querySelector(".wrapper");
+    const resp = await fetch(
+      `https://node-rest-api-zs36.onrender.com/api/users/verify/${
+        window.location.href.split("?")[1]
+      }`
+    );
+    if (resp.status === 404) {
+      const message = "User not found...";
+      document.querySelector("h1").textContent = message;
+    }
+    if (resp.status === 200) {
+      const message = "Email has been verified successfuly";
+      document.querySelector("h1").textContent = message;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
