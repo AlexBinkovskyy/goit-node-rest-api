cbbedea527231f7f640719c430e7391d9c1ccdd5
AlexBinkovskyy <alex_bin@ukr.net>
2024-05-08 20:42:15 +0300

    change url

--- public/html/script.js
+++ public/html/script.js
@@ -13,10 +13,10 @@ window.onload = async () => {
     if (resp.status === 200) {
       const message = "Email has been verified successfuly";
       document.querySelector("h1").textContent = message;
-      const timeoutMilliseconds = 3000;
+      const timeoutMilliseconds = 2000;
 
       const timeoutId = setTimeout(() => {
-        window.location.href = "http://ukr.net";
+        window.location.href = "http://google.com";
       }, timeoutMilliseconds);
     }
   } catch (err) {
